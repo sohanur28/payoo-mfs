df288fa54efd81f1557d78af20d017e3a2f36cb3
sohanur28 <sohanur28@gmail.com>
2025-06-21 18:05:11 +0600

    verify pin & add money

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@
 /*
 step-1: add event handler prevent reload form submit
 step-2: get money to be added to the account balance
+step-3: verify the pin number
  */
 
 // step-1: add an event handler to the add money button inside the form
@@ -16,7 +17,27 @@ document
     console.log(addMoneyInput);
 
     // get the pin number provided
-    const pinNumberInput = document.getElementById('input-pin-number').value;
+    const pinNumberInput = document.getElementById("input-pin-number").value;
     console.log(pinNumberInput);
-    
+
+    // step-3: verify pin number
+    // wrong way to validate pin number
+    if (pinNumberInput === "1234") {
+      console.log("adding money to your account");
+
+      // step-4: get the current balance
+      const balance = document.getElementById("account-balance").innerText;
+      console.log(typeof balance);
+
+      // step-5: add addMoneyInput with balance
+      const addMoneyNumber = parseFloat(addMoneyInput);
+      const balanceNumber = parseFloat(balance);
+      const newBalance = addMoneyNumber + balanceNumber;
+      console.log(newBalance);
+
+      // step-6: update the balance in the UI/DOM
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("please try again");
+    }
   });
